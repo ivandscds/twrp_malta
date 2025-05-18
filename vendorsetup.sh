b7c0b0ac8516412eeae6b4d2dd4a75d58eb82984
ivandscds <119276005+ivandscds@users.noreply.github.com>
2025-05-18 20:46:16 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_malta-user
-add_lunch_combo omni_malta-userdebug
-add_lunch_combo omni_malta-eng
+add_lunch_combo twrp_malta-user
+add_lunch_combo twrp_malta-userdebug
+add_lunch_combo twrp_malta-eng
